7dc8f2f7210002a12b4055291490c65658f20a75
Sebastian Hoß <sebastian.hoss@shoss.de>
2013-12-22 19:32:29 +0100

    Initial import
    Signed-off-by: Sebastian Hoß <sebastian.hoss@shoss.de>

---
+++ src/main/java/com/github/sebhoss/nullanalysis/Nullsafe.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2013 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+package com.github.sebhoss.nullanalysis;
+
+import javax.annotation.Nullable;
+
+/**
+ * Utility classes which helps working with legacy (nullable) APIs.
+ */
+public final class Nullsafe {
+
+    /**
+     * @param reference
+     *            A possible <code>null</code> reference.
+     * @return Either the reference itself, or an {@link NullPointerException}, in case the reference was
+     *         <code>null</code>.
+     */
+    public static <T> T nullsafe(@Nullable final T reference) {
+        if (reference != null) {
+            return reference;
+        }
+
+        throw new NullPointerException(); // NOPMD - we want to throw NPE here
+    }
+
+    /**
+     * @param reference
+     *            A possible <code>null</code> reference.
+     * @param message
+     *            The exception message to throw.
+     * @return Either the reference itself, or an {@link NullPointerException}, in case the reference was
+     *         <code>null</code>.
+     */
+    public static <T> T nullsafe(@Nullable final T reference, final String message) {
+        if (reference != null) {
+            return reference;
+        }
+
+        throw new NullPointerException(message); // NOPMD - we want to throw NPE here
+    }
+
+    private Nullsafe() {
+        // utility class
+    }
+
+}
---
+++ src/main/java/com/github/sebhoss/nullanalysis/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2013 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+/**
+ * Code related to <code>null</code> analysis.
+ */
+@NotNullByDefault
+package com.github.sebhoss.nullanalysis;
